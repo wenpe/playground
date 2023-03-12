137e4c1094340cc7c25d56417915cd046c40a2bc
wenpe <wenpe@wenpes-MacBook-Air.local>
2023-03-12 14:20:27 +0900

    [add] example for break_loop

---
+++ ansible/example/break_loop/main.py
@@ -0,0 +1,12 @@
+loop_items = [
+    "not target",
+    "not target",
+    "target",
+    "not target",
+    "not target",
+]
+
+for item in loop_items:
+    print(item)
+    if item == "target":
+        break
